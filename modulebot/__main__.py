2813fa9e94706c8f86afe8e83bf0a321b22bbc4c
shotnothing <shotnothing@protonmail.ch>
2022-08-06 22:37:15 +0800

    added docstrings, testing out types of update and context

--- modulebot/__main__.py
+++ modulebot/__main__.py
@@ -1,3 +1,10 @@
+"""Summary
+
+Attributes:
+    logger (TYPE): Description
+    PORT (TYPE): Description
+    TOKEN (TYPE): Description
+"""
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 import logging
@@ -13,29 +20,46 @@ logger = logging.getLogger(__name__)
 # Fetch token
 TOKEN = os.environ.get('token', None)
 
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
 def start(update, context):
-    """Send a message when the command /start is issued."""
+    """Start command handler
+    
+    Args:
+        update (TYPE): Description
+        context (TYPE): Description
+    """
     update.message.reply_text('Hi!')
 
 def help(update, context):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
+    """Summary
+    
+    Args:
+        update (TYPE): Description
+        context (TYPE): Description
+    """
+    update.message.reply_text(type(update))
+    update.message.reply_text(type(context))
 
-def echo(update, context):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+def dont_understand(update, context):
+    """Summary
+    
+    Args:
+        update (TYPE): Description
+        context (TYPE): Description
+    """
+    update.message.reply_text("Sorry, I don't understand {update.message.text}")
 
 def error(update, context):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
+    """Summary
+    
+    Args:
+        update (TYPE): Description
+        context (TYPE): Description
+    """
+    logger.warning('[Error] Update "{update}" caused error "{context.error}"')
 
 def main():
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
-    # Make sure to set use_context=True to use the new context based callbacks
-    # Post version 12 this will no longer be necessary
+    """Summary
+    """
     updater = Updater(TOKEN, use_context=True)
 
     # Get the dispatcher to register handlers
@@ -46,20 +70,17 @@ def main():
     dp.add_handler(CommandHandler("help", help))
 
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_handler(MessageHandler(Filters.text, dont_understand))
 
-    # log all errors
+    # log errors
     dp.add_error_handler(error)
 
-    # Start the Bot
+    # start bot
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
     updater.bot.setWebhook('https://modulebotservice.herokuapp.com/' + TOKEN)
 
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 if __name__ == '__main__':
