a1e1027b5c3bad6b95bedd3bece0ca93d5071bee
shotnothing <shotnothing@protonmail.ch>
2022-08-06 22:03:58 +0800

    testing heroku integration

--- modulebot/modulebot.py
+++ modulebot/modulebot.py
@@ -55,7 +55,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
-    updater.bot.setWebhook('https://yourherokuappname.herokuapp.com/' + TOKEN)
+    updater.bot.setWebhook('https://modulebotservice.herokuapp.com/' + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
